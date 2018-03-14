e46fae23e8f2cfac24a930021a8c8ccb2cc1cc43
john <ismirnov45@gmail.com>
2018-03-15 01:53:24 +0300

    Improving class World, adding some methods' interfaces

--- model.py
+++ model.py
@@ -1,15 +1,31 @@
-def singleton(class_):
-    instances = {}
-
-    def get_instance(*args, **kwargs):
-        if class_ not in instances:
-            instances[class_] = class_(*args, **kwargs)
-        return instances[class_]
-
-    return get_instance
-
-
-@singleton
 class World:
-    def __init__(self, size_of_world=(3, 5), number_of_players=2):
+    def __init__(self, size_of_world=(3, 5),  # load from file in init?
+                 number_of_generations=-1, mode='every_turn', file=None):
+        if type(size_of_world) != tuple:
+            return  # some kind of exception here
+
+        if type(number_of_generations) != int:
+            return  # and here
+
+        if mode != 'every_turn' and mode != 'just_result':
+            return  # and here, too
+
+        self._width, self._height = size_of_world
+        self._number_of_generations = number_of_generations
+        self._current_generation = 0
+        self._field = [['.' for j in range(self._width)] for i in
+                       range(self._height)]
+
+        if file is not None:
+            self._load_state(file)
+
+    def _load_state(self, file=None):
+        if file is None:
+            return
+
+    def _save_state(self, file=None):
+        if file is None:
+            return
+
+    def update(self):  # what about rules? maybe classes or functions?
         return
