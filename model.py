8262f957b88101aaea0eaecd9bf16a2c29924274
john <ismirnov45@gmail.com>
2018-03-17 07:35:57 +0300

    Making update() method in World class working and adding rules

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class World:
     def __init__(self, size_of_world=(3, 5),  # load from file in init?
                  number_of_generations=-1, mode='every_turn', file=None):
@@ -13,6 +16,7 @@ class World:
         self._width, self._height = size_of_world
         self._number_of_generations = number_of_generations
         self._current_generation = 0
+        self._mode = mode
         self._field = [['.' for j in range(self._width)] for i in
                        range(self._height)]
 
@@ -27,5 +31,33 @@ class World:
         if file is None:
             return
 
-    def update(self):  # what about rules? maybe classes or functions?
+    def _rules(self, cell='.', neighbors=None):
         return
+
+    def update(self):
+        if self._current_generation < self._number_of_generations or \
+                self._number_of_generations == -1:
+            self._current_generation += 1
+
+            new_field = [['.' for j in range(self._width)] for i in
+                         range(self._height)]
+
+            for i in range(self._height):
+                for j in range(self._width):
+                    neighbors = defaultdict(int)
+
+                    for di in range(-1, 2):
+                        for dj in range(-1, 2):
+                            if di == dj == 0:
+                                continue
+
+                    new_field[i][j] = self._rules(self._field[i][j], neighbors)
+
+            self._field = new_field
+
+            if self._mode == 'every_turn' or \
+                    self._current_generation == self._number_of_generations:
+                self._save_state()
+                return True
+
+        return False
