f1df9bfc54834f2fbf444294c67a3edb0ca0988c
john <ismirnov45@gmail.com>
2018-03-20 15:57:41 +0300

    Improving argparse help

--- main.py
+++ main.py
@@ -7,26 +7,27 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--input_file', type=str, default='stdin',
-                        help='Path to input file')
+                        help='Path to input file(stdin as default)')
 
     parser.add_argument('--output_file', type=str, default='stdout',
-                        help='Path to output file')
+                        help='Path to output file(stdout as default)')
 
     parser.add_argument('--mode', type=str, default='every_turn',
-                        help='Showing mode(every_turn or just_once)')
+                        help='Showing mode(every_turn or just_once)\n' +
+                             'every_turn as default')
 
     parser.add_argument('--graphical_mode', type=str, default='True',
-                        help='Graphical mode(\'False\' if only text).' +
+                        help='Graphical mode(\'False\' if only text)\n' +
                              'For normal usage you need pygame')
 
     parser.add_argument('--height_of_world', type=int, default=1,
-                        help='Height of World')
+                        help='Height of World(1 as default)')
 
     parser.add_argument('--width_of_world', type=int, default=1,
-                        help='Width of World')
+                        help='Width of World(1 as default)')
 
     parser.add_argument('--number_of_generations', type=int, default=-1,
-                        help='Number of generations(infinite in default)' +
+                        help='Number of generations(infinite as default)\n' +
                              'Should be infinite only in graphical mode')
 
     args = parser.parse_args()
